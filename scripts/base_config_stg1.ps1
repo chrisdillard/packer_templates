45f53415b5c6fc4177eafefe4c160ca36b656a8e
Chris Dillard <chrisdillard@outlook.com>
2017-07-03 23:27:14 -0500

    Move network location popup to unattend file(Issue #3) and disable hibernation on all Windows OS. Knocks a couple GB off the template.

--- scripts/base_config_stg1.ps1
+++ scripts/base_config_stg1.ps1
@@ -9,11 +9,6 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PSWindowsUpdate -RequiredVersion 1.5.2.6 -Force
 Import-Module PSWindowsUpdate
 
-#Annoying Network Popup
-New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff" -Force
-$ifaceinfo = Get-NetConnectionProfile
-Set-NetConnectionProfile -InterfaceIndex $ifaceinfo.InterfaceIndex -NetworkCategory Private
-
 #Set Stage 2 Script to RunOnce
 Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name 'Stage_2' -Value "c:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -noexit a:\base_config_stg2.ps1" -Force
 
