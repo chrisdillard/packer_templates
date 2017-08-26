7aede5055d54846d84bb776c47e7161a6a3ce6d1
Chris Dillard <chrisdillard@outlook.com>
2017-08-25 21:15:57 -0500

    Added CentOS support, improved partition layout, moved Linus updates to provisioner, and other improvements.

--- scripts/base_config_stg1.ps1
+++ scripts/base_config_stg1.ps1
@@ -3,25 +3,28 @@
 #choco feature enable --name allowGlobalConfirmation
 #choco feature disable --name checksumFiles
 
+#Set static IP if needed#
+#New-NetIPAddress -InterfaceIndex 1 -IPAddress 10.202.138.99 -PrefixLength 26 -DefaultGateway 10.202.138.126
+#Set-DnsClientServerAddress -InterfaceIndex 1 -ServerAddresses 10.202.133.1, 10.204.133.1
+
 #Set timezone#
-Set-TimeZone -Name "Central Standard Time"
+#Set-TimeZone -Name "Central Standard Time"
 
 #Install NuGet#
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 
 #Install PSWindowsUpdate Module#
 #choco install pswindowsupdate
-Install-Module -Name PSWindowsUpdate -RequiredVersion 1.6.0.3 -Force
+Install-Module -Name PSWindowsUpdate -RequiredVersion 1.6.1.1 -Force
 Import-Module PSWindowsUpdate
 
 #Install Chef-Client#
-#. { iwr -useb https://omnitruck.chef.io/install.ps1 } | iex; install -version 13.1.31
+. { iwr -useb https://omnitruck.chef.io/install.ps1 } | iex; install -version 13.3.42
 
 #Set Stage 2 Script to RunOnce#
 Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name 'Stage_2' -Value "c:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -noexit a:\base_config_stg2.ps1" -Force
 
 #Install .net 3.5#
-#Install-WindowsFeature Net-Framework-Core -source "D:\sources\sxs"
 Dism /online /enable-feature /featurename:NetFx3 /All /Source:D:\sources\sxs /LimitAccess
 
 #Installing Windows Updates#
--- scripts/install_tools.ps1
+++ scripts/install_tools.ps1
@@ -1,1 +1,11 @@
-Start-Process msiexec.exe -Wait -ArgumentList '/I C:\build\VMwareTools64.msi /qn REBOOT=R'
+Write-Host "Installing VMware Tools"
+$tools = Start-Process msiexec.exe -Wait -ArgumentList '/I C:\Windows\Temp\VMwareTools64.msi /qn REBOOT=R'
+
+if ($tools.ExitCode -eq 0) {
+    Write-Host "Tools complete"
+} elseif ($tools.ExitCode -eq 3010) {
+    Write-Host "Tools complete. Can I haz reboot?"
+} else {
+    Write-Host "You have failed me: ExitCode=$($tools.ExitCode), Log=C:\Windows\Temp\vmware_tools.log"
+    exit $tools.ExitCode
+}
