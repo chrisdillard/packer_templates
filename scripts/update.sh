2838cfc5c944a122375c0940dfc440f335983467
Chris Dillard <chrisdillard@users.noreply.github.com>
2017-09-04 00:25:07 -0500

    Finalized linux partition layout, reset passwords with sha512 hash, hostname and epel logic for rhel and cent, disabled root ssh.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,17 @@
+yum install wget -y
+
 if [ -n "$( cat /etc/*-release | grep "CentOS" )" ]
 then
-    yum --enablerepo=extras install epel-release -y
+    yum install epel-release -y
+elif [ -n "$( cat /etc/*-release | grep "(Santiago)" )" ]
+then
+    wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+    yum localinstall epel-release-latest-6.noarch.rpm -y
+else
+    wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    yum localinstall epel-release-latest-7.noarch.rpm -y
 fi
 
-yum install open-vm-tools wget curl rsyslog man man-pages -y
+yum install open-vm-tools curl rsyslog man man-pages bind-utils -y
 yum upgrade -y
 yum clean all
